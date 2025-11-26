638d92c0bdc69576f0d9d775341db7d8fc8b5853
eduardonavigogroup <eduardo.fuentealbau@gmail.com>
2025-11-26 14:30:42 -0300

    Herencia: Terminamos de revisar el tutorial de herencia

--- POO/Herencia.py
+++ POO/Herencia.py
@@ -16,3 +16,32 @@ class Student(Person):
 
 y = Student("Mike", "Olsen")
 y.printname()
+
+
+#En este ejemplo se muestra como agregar un __init__() a la clase hija
+class Student(Person):
+    def __init__(self, fname, lname):
+        Person.__init__(self, fname, lname)
+
+# En este ejemplo se muestra como usar la funcion super() para heredar la clase padre
+class Student(Person):
+    def __init__(self, fname, lname):
+        super().__init__(fname, lname)
+        self.graduationyear = 2024
+
+x = Student("Mike", "Olsen")
+print(x.graduationyear)
+
+# En este ejemplo se muestra como usar la funcion super() para heredar la clase padre
+class Student(Person):
+    def __init__(self, fname, lname, year):
+        super().__init__(fname, lname)
+        self.graduationyear = year
+
+    def welcome(self):
+        print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+    
+
+x = Student("Mike", "Olsen", 2016)
+print(x.graduationyear)
+x.welcome()
